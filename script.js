e3db9c5826297824efd18470e51af47a831e4901
Elliot Trebajo <trebaell@gmail.com>
2020-02-11 17:40:31 -0500

    basics complete

--- script.js
+++ script.js
@@ -24,6 +24,20 @@
  * Start Helper Functions
  * 
  */
+function goTo1() {
+    console.log("clicked1")
+    document.querySelector('#section1').scrollIntoView()
+}
+
+function goTo2() {
+    console.log("clicked2")
+    document.querySelector('#section2').scrollIntoView()
+}
+
+function goTo3() {
+    console.log("clicked3")
+    document.querySelector('#section3').scrollIntoView()
+}
 
 
 
@@ -34,6 +48,19 @@
  */
 
 // build the nav
+const nav1 = document.createElement('li')
+const nav2 = document.createElement('li')
+const nav3 = document.createElement('li')
+nav1.className = "menu__link"
+nav2.className = "menu__link"
+nav3.className = "menu__link"
+const nav = document.querySelector('#navbar__list')
+nav1.textContent = 'first'
+nav2.textContent = 'second'
+nav3.textContent = 'third'
+nav.appendChild(nav1)
+nav.appendChild(nav2)
+nav.appendChild(nav3)
 
 
 // Add class 'active' to section when near top of viewport
@@ -49,6 +76,9 @@
  */
 
 // Build menu 
+nav1.addEventListener('click', goTo1)
+nav2.addEventListener('click', goTo2)
+nav3.addEventListener('click', goTo3)
 
 // Scroll to section on link click
 
